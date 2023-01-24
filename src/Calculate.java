b5829cdf788fd7cd4689331c4fce62c7fddd0f51
Nick-XYZ <123240062+Nick-XYZ@users.noreply.github.com>
2023-01-24 13:36:15 +0100

    Create Calculate.java

---
+++ src/Calculate.java
@@ -0,0 +1,8 @@
+public class Calculate {
+    public static void main(String[] args) {
+        System.out.println("This is the Git checkpoint task 1.1");
+      System.out.println("Dev Branch Calculate");
+
+        CheckTest.checkTest();
+    }
+}
